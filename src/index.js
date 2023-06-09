d1cc1a622c4d5391caa5f3201e9ff9d4090f8e07
Fabiola Granados <faby.granados@gmail.com>
2023-06-09 10:42:38 -0700

    Boton de limpiar texto funciona para limpiar las caracteristicas y el textarea

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import analyzer from './analyzer.js';
-const contadorCaracteres = document.querySelector('textarea[name="user-input"]');
+const textarea = document.querySelector('textarea');
 
-contadorCaracteres.addEventListener("keyup", function(){
-    const mensaje = contadorCaracteres.value;
+textarea.addEventListener("keyup", function(){
+    const mensaje = textarea.value;
     const caracteres = analyzer.getCharacterCount(mensaje);
     const contador = document.querySelector(".caracteres");
     contador.innerHTML = caracteres;
+});
+
+
+
+
+const resetMetricas = document.querySelector('#reset-button');
+
+resetMetricas.addEventListener('click', function() {
+  textarea.value = '';
+  let caracteres = document.querySelector('.caracteres');
+  caracteres.innerHTML = "0";
 });
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
