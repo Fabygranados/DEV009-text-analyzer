11d2ad3541b094d5103bc2a82a806726c57a2019
Fabiola Granados <faby.granados@gmail.com>
2023-06-09 17:12:39 -0700

    funcion del la longitud promedio de las palabras y el boton limpia adecuadamente los parametros

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ textarea.addEventListener("keyup", function(){
     const mensaje = textarea.value;
     const caracteres = analyzer.getCharacterCount(mensaje);
     const wordCount = analyzer.getWordCount(mensaje);
+    const noEspacios = analyzer.getCharacterCountExcludingSpaces(mensaje);
+    const longitudProm = analyzer.getAverageWordLength(mensaje);
+    const lonPromedio = document.querySelector(".Promedio");
+    const sinEspacios = document.querySelector(".sinEspacios");
     const Palabras = document.querySelector(".palabras");
     const contador = document.querySelector(".caracteres");
+    lonPromedio.innerHTML= longitudProm;
+    sinEspacios.innerHTML = noEspacios;
     Palabras.innerHTML = wordCount;
     contador.innerHTML = caracteres;
 });
@@ -20,5 +26,9 @@ resetMetricas.addEventListener('click', function() {
   caracteres.innerHTML = "0";
   let palabras = document.querySelector(".palabras");
   palabras.innerHTML = "0";
+  let sinEspacios = document.querySelector(".sinEspacios");
+  sinEspacios.innerHTML = "0";
+  let lonPromedio = document.querySelector(".Promedio");
+  lonPromedio.innerHTML = "0";
 });
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
