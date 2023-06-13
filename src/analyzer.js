0228e6f0efced919268aff1a9d1ba42871ab0773
Fabiola Granados <faby.granados@gmail.com>
2023-06-13 11:11:07 -0700

    Se concliye el readme

--- src/analyzer.js
+++ src/analyzer.js
@@ -10,7 +10,7 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    const cleanText = text.replace(/[^\w]/g, '');
+    const cleanText = text.replace(/[^\w]/g, ''); //expresion regular para limpiar texto de estacios y signos de puntuacion
     const contadorCar = cleanText.length;
     return contadorCar;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
@@ -20,7 +20,7 @@ const analyzer = {
     let sumaLongitud = 0;
     let cantidadPalabras = 0;
 
-    for (let i = 0; i < palabras.length; i++){
+    for (let i = 0; i < palabras.length; i ++){ //
       sumaLongitud += palabras[i].length;
       cantidadPalabras++;
     }
